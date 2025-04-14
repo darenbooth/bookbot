6dbbdda55d97afbd51ba691a64695301f32ee203
Daren Booth <57602377+darenbooth@users.noreply.github.com>
2025-04-14 12:41:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from stats import get_num_words, get_char_count, sort_on, sort_dict
+from stats import char_dict_to_sorted_dict_list, text_to_num_words
 
 def main():
-    get_num_words()
+    book_path = "books/frankenstein.txt"
 
-    char_count = get_char_count()
-    sorted_list = sort_dict(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {text_to_num_words()} total words")
+    print("--------- Character Count -------")
 
+    sorted_list = char_dict_to_sorted_dict_list()
+    char_dict_to_sorted_dict_list()
     for char_dict in sorted_list:
         char = char_dict["character"]
         count = char_dict["count"]
         if char.isalpha():
             print(f"{char}: {count}")
+            
+    print("============= END ===============")
 
 main()
