0cb4e860d06d8c0035f58f15620a0018ec2416e2
darenbooth <darenbooth22@gmail.com>
2025-04-14 14:09:13 -0500

    edit main.py, stats.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from stats import char_dict_to_sorted_dict_list, text_to_num_words
+import sys
+from stats import char_dict_to_sorted_dict_list, text_to_num_words, get_book_text
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+
+    #book_path = "books/frankenstein.txt"
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {text_to_num_words()} total words")
+    print(f"Found {text_to_num_words(book_path)} total words")
     print("--------- Character Count -------")
 
-    sorted_list = char_dict_to_sorted_dict_list()
-    char_dict_to_sorted_dict_list()
+    sorted_list = char_dict_to_sorted_dict_list(book_path)
+    char_dict_to_sorted_dict_list(book_path)
     for char_dict in sorted_list:
         char = char_dict["character"]
         count = char_dict["count"]
--- stats.py
+++ stats.py
@@ -1,19 +1,19 @@
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+def get_book_text(path):
+    with open(path) as f:
         file_contents = f.read()
     return file_contents
 
-def text_to_num_words():
+def text_to_num_words(path):
     word_count = 0
-    book_text = get_book_text()
+    book_text = get_book_text(path)
     words = book_text.split()
     for word in words:
         word_count += 1
     return word_count
 
-def text_to_num_chars():
+def text_to_num_chars(path):
     char_count_dict = {}
-    chars = get_book_text().lower()
+    chars = get_book_text(path).lower()
     for char in chars:
         if char in char_count_dict:
             char_count_dict[char] += 1
@@ -24,8 +24,8 @@ def text_to_num_chars():
 def sort_dict(dict):
     return dict["count"]
 
-def char_dict_to_sorted_dict_list():
-    char_count_dict = text_to_num_chars()
+def char_dict_to_sorted_dict_list(path):
+    char_count_dict = text_to_num_chars(path)
     sorted_list_dict = []
     for char, count in char_count_dict.items():
         sorted_list_dict.append({"character": char, "count": count})
